8eeb7987b46552cd8be324e6b76960b15661b7a9
Thyagoj7 <t.y_josef7@hotmail.com>
2024-06-19 19:11:33 -0300

    iniciando projeto

---
+++ Back-end/api/src/main/java/com/projetocodigocerto/api/ApiProjetocodigocertoApplication.java
@@ -0,0 +1,13 @@
+package com.projetocodigocerto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjetocodigocertoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProjetocodigocertoApplication.class, args);
+	}
+
+}
